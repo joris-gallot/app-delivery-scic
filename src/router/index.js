24fbf2663480325e9e8da1f40e7a41a13c5a5454
Joris <you@example.com>
2020-12-03 14:46:09 +0100

    auth guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -9,12 +11,26 @@ const routes = [
   {
     path: '/',
     name: 'login',
+    meta: {
+      requiresNoAuth: true,
+    },
     component: Login,
   },
   {
     path: '/register',
+    meta: {
+      requiresNoAuth: true,
+    },
     name: 'register',
     component: Register,
+  },
+  {
+    path: '/home',
+    meta: {
+      requiresAuth: true,
+    },
+    name: 'home',
+    component: Home,
   },
 ]
 
@@ -22,6 +38,28 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.state.isUserLoggedIn) {
+      next()
+    } else {
+      next({
+        name: 'login',
+      })
+    }
+  } else if (to.matched.some(record => record.meta.requiresNoAuth)) {
+    if (store.state.isUserLoggedIn) {
+      next({
+        name: 'home',
+      })
+    } else {
+      next()
+    }
+  } else {
+    next() // make sure to always call next()!
+  }
 })
 
 export default router
